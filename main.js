d0b41e60d173b76de03dc65b5add0c050985a86d
vanessacreis <vanessacreis@live.com>
2022-05-16 17:51:28 -0300

    Javascript do menu implementado e funcionando

--- main.js
+++ main.js
@@ -3,6 +3,31 @@ window.addEventListener("scroll", onScroll);
 function onScroll() {
   showNavOnScroll();
   showBackToTopBTNOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
 function showNavOnScroll() {
